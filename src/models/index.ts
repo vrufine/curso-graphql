06b138dc4c862b8662d3fc7bb132dfa18d8efa59
Vinicius Rufine <vsrufine@gmail.com>
2018-02-11 20:38:04 -0200

    Aula 42
    Operators aliases

--- src/models/index.ts
+++ src/models/index.ts
@@ -11,6 +11,8 @@ let db = null;
 
 if (!db) {
   db = {};
+  const operatorsAliases = false;
+  config = Object.assign({ operatorsAliases }, config);
   const sequelize: Sequelize.Sequelize = new Sequelize(
     config.database,
     config.username,
