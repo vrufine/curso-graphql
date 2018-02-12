131843e210565804bb9d6eb6440ae839dac3da68
Vinicius Rufine <vsrufine@gmail.com>
2018-02-12 20:10:35 -0200

    Aula 54
    User resolvers: Mutations

--- src/graphql/resources/user/user.resolvers.ts
+++ src/graphql/resources/user/user.resolvers.ts
@@ -1,7 +1,8 @@
-import { DbConnetion } from "../../../interfaces/DbConnectionInterface";
-import { UserInstance } from "../../../models/UserModel";
 import { GraphQLResolveInfo } from "graphql";
 import { Transaction } from "sequelize";
+
+import { DbConnetion } from "../../../interfaces/DbConnectionInterface";
+import { UserInstance } from "../../../models/UserModel";
 
 export const userResolvers = {
   User: {},
@@ -30,64 +31,64 @@ export const userResolvers = {
           return user;
         });
     }
-  }
-  // Mutation: {
-  //   createUser(
-  //     parent,
-  //     { input },
-  //     { db }: { db: DbConnetion },
-  //     info: GraphQLResolveInfo
-  //   ) {
-  //     return db.sequelize.transaction((t: Transaction) => {
-  //       return db.User.create(input, { transaction: t });
-  //     });
-  //   },
-  //   updateUser(
-  //     parent,
-  //     { id, input },
-  //     { db }: { db: DbConnetion },
-  //     info: GraphQLResolveInfo
-  //   ) {
-  //     id = parseInt(id);
-  //     return db.sequelize.transaction((t: Transaction) => {
-  //       return db.User.findById(id)
-  //         .then((user: UserInstance) => {
-  //           if (!user) throw new Error(`User with id ${id} not found!`);
-  //           return user.update(input, { transaction: t });
-  //         });
-  //     });
-  //   },
-  //   updateUserPassword(
-  //     parent,
-  //     { id, input },
-  //     { db }: { db: DbConnetion },
-  //     info: GraphQLResolveInfo
-  //   ) {
-  //     id = parseInt(id);
-  //     return db.sequelize.transaction((t: Transaction) => {
-  //       return db.User.findById(id)
-  //         .then((user: UserInstance) => {
-  //           if (!user) throw new Error(`User with id ${id} not found!`);
-  //           return user.update(input, { transaction: t })
-  //             .then((user: UserInstance) => !!user);
-  //         });
-  //     });
-  //   },
-  //   deleteUser(
-  //     parent,
-  //     { id },
-  //     { db }: { db: DbConnetion },
-  //     info: GraphQLResolveInfo
-  //   ) {
-  //     id = parseInt(id);
-  //     return db.sequelize.transaction((t: Transaction) => {
-  //       return db.User.findById(id)
-  //         .then((user: UserInstance) => {
-  //           if (!user) throw new Error(`User with id ${id} not found!`);
-  //           return user.destroy({ transaction: t })
-  //             .then(user => !!user);
-  //         });
-  //     });
-  //   },
-  // },
+  },
+  Mutation: {
+    createUser(
+      parent,
+      { input },
+      { db }: { db: DbConnetion },
+      info: GraphQLResolveInfo
+    ) {
+      return db.sequelize.transaction((t: Transaction) => {
+        return db.User.create(input, { transaction: t });
+      });
+    },
+    updateUser(
+      parent,
+      { id, input },
+      { db }: { db: DbConnetion },
+      info: GraphQLResolveInfo
+    ) {
+      id = parseInt(id);
+      return db.sequelize.transaction((t: Transaction) => {
+        return db.User.findById(id)
+          .then((user: UserInstance) => {
+            if (!user) throw new Error(`User with id ${id} not found!`);
+            return user.update(input, { transaction: t });
+          });
+      });
+    },
+    updateUserPassword(
+      parent,
+      { id, input },
+      { db }: { db: DbConnetion },
+      info: GraphQLResolveInfo
+    ) {
+      id = parseInt(id);
+      return db.sequelize.transaction((t: Transaction) => {
+        return db.User.findById(id)
+          .then((user: UserInstance) => {
+            if (!user) throw new Error(`User with id ${id} not found!`);
+            return user.update(input, { transaction: t })
+              .then((user: UserInstance) => !!user);
+          });
+      });
+    },
+    deleteUser(
+      parent,
+      { id },
+      { db }: { db: DbConnetion },
+      info: GraphQLResolveInfo
+    ) {
+      id = parseInt(id);
+      return db.sequelize.transaction((t: Transaction) => {
+        return db.User.findById(id)
+          .then((user: UserInstance) => {
+            if (!user) throw new Error(`User with id ${id} not found!`);
+            return user.destroy({ transaction: t })
+              .then(user => !!user);
+          });
+      });
+    },
+  },
 };
