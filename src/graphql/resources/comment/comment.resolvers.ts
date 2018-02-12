3c70078b1591449a404a7be8d92a5a17029dc118
Vinicius Rufine <vsrufine@gmail.com>
2018-02-12 20:32:28 -0200

    Aula 59
    Comment resolvers: Mutations

--- src/graphql/resources/comment/comment.resolvers.ts
+++ src/graphql/resources/comment/comment.resolvers.ts
@@ -1,5 +1,7 @@
 import { DbConnetion } from "../../../interfaces/DbConnectionInterface";
 import { GraphQLResolveInfo } from "graphql";
+import { Transaction } from "sequelize";
+import { CommentInstance } from "../../../models/CommentModel";
 
 export const commentResolvers = {
   Comment: {
@@ -34,5 +36,48 @@ export const commentResolvers = {
         offset
       });
     }
+  },
+  Mutation: {
+    createComment(
+      parent,
+      { input },
+      { db }: { db: DbConnetion },
+      info: GraphQLResolveInfo
+    ) {
+      return db.sequelize.transaction((t: Transaction) => {
+        return db.Comment.create(input, { transaction: t });
+      });
+    },
+    updateComment(
+      parent,
+      { id, input },
+      { db }: { db: DbConnetion },
+      info: GraphQLResolveInfo
+    ) {
+      id = parseInt(id);
+      return db.sequelize.transaction((t: Transaction) => {
+        return db.Comment.findById(id)
+          .then((comment: CommentInstance) => {
+            if (!comment) throw new Error(`Comment with id ${id} not found!`);
+            return comment.update(input, { transaction: t });
+          })
+      });
+    },
+    deleteComment(
+      parent,
+      { id },
+      { db }: { db: DbConnetion },
+      info: GraphQLResolveInfo
+    ) {
+      id = parseInt(id);
+      return db.sequelize.transaction((t: Transaction) => {
+        return db.Comment.findById(id)
+          .then((comment: CommentInstance) => {
+            if (!comment) throw new Error(`Comment with id ${id} not found!`);
+            return comment.destroy({ transaction: t })
+              .then(comment => !!comment);
+          })
+      });
+    },
   }
 };
