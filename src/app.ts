6a1441695cc4d60c755f61dcd61b33e829cae0f9
Vinicius Rufine <vsrufine@gmail.com>
2018-02-12 19:53:54 -0200

    Aula 52
    DbConnection no context dos resolvers

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,26 @@ import * as express from 'express';
 import * as graphqlHTTP from 'express-graphql';
 
 import schema from './graphql/schema';
+import db from './models';
 
 class App {
   public express: express.Application;
-
   constructor() {
     this.express = express();
     this.middleware();
   }
-
   private middleware() {
     this.express.use('/graphql',
-      graphqlHTTP({
+      (req: express.Request, res: express.Response, next: express.NextFunction) => {
+        req['context'] = {};
+        req['context']['db'] = db;
+        next();
+      },
+      graphqlHTTP((req) => ({
         schema,
-        graphiql: process.env.NODE_ENV === 'development'
-      })
+        graphiql: process.env.NODE_ENV === 'development',
+        context: req['context']
+      }))
     );
   }
 }
