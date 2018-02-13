19848f4884da3e45d9ca2a3a3b6550a066f49eba
Vinicius Rufine <vsrufine@gmail.com>
2018-02-12 22:46:00 -0200

    72. extractJwtMiddleware

---
+++ src/middlewares/extract-jwt.middleware.ts
@@ -0,0 +1,35 @@
+import * as jwt from 'jsonwebtoken';
+import { RequestHandler, Request, Response, NextFunction } from 'express';
+
+import { JWT_SECRET } from '../utils/utils';
+
+import db from './../models';
+import { UserInstance } from '../models/UserModel';
+
+export const extractJwtMiddleware = (): RequestHandler => {
+  return (req: Request, res: Response, next: NextFunction): void => {
+    let authorization: string = req.get('Authorization');
+    let token: string = authorization ? authorization.split(' ')[1] : undefined;
+
+    req['context'] = {};
+    req['context']['authorization'] = authorization;
+
+    if (!token) return next();
+
+    jwt.verify(token, JWT_SECRET, (err, decoded: any) => {
+      if (err) return next();
+
+      db.User.findById(decoded.sub, {
+        attributes: ['id', 'email']
+      }).then((user: UserInstance) => {
+        if (user) {
+          req['context']['user'] = {
+            id: user.get('id'),
+            email: user.get('email')
+          }
+        }
+        return next();
+      });
+    });
+  };
+};
