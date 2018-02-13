5e495c45c379e8abecd98d7e6705e4d54d16a541
Vinicius Rufine <vsrufine@gmail.com>
2018-02-13 12:20:34 -0200

    95. DataLoaders no context dos resolvers

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,17 @@ import * as graphqlHTTP from 'express-graphql';
 import db from './models';
 import schema from './graphql/schema';
 import { extractJwtMiddleware } from './middlewares/extract-jwt.middleware';
+import { DataLoaderFactory } from './graphql/dataloaders/DataLoaderFactory';
 
 class App {
   public express: express.Application;
+  private dataLoaderFactory: DataLoaderFactory;
   constructor() {
     this.express = express();
+    this.init();
+  }
+  private init() {
+    this.dataLoaderFactory = new DataLoaderFactory(db);
     this.middleware();
   }
   private middleware() {
@@ -16,6 +22,7 @@ class App {
       extractJwtMiddleware(),
       (req: express.Request, res: express.Response, next: express.NextFunction) => {
         req['context']['db'] = db;
+        req['context']['dataloaders'] = this.dataLoaderFactory.getLoaders();
         next();
       },
       graphqlHTTP((req) => ({
--- src/interfaces/ResolverContextInterface.ts
+++ src/interfaces/ResolverContextInterface.ts
@@ -1,8 +1,10 @@
 import { DbConnetion } from "./DbConnectionInterface";
 import { AuthUser } from "./AuthUserInterface";
+import { DataLoaders } from "./DataLoadersInterface";
 
 export interface ResolverContext {
   db?: DbConnetion;
   authorization?: string;
   authUser?: AuthUser;
+  dataloaders?: DataLoaders;
 }
