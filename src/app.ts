3fa910d9f1e76e5b89ccbf8257a99f753053fbfa
Vinicius Rufine <vsrufine@gmail.com>
2018-02-13 12:59:20 -0200

    100. Instância RequestedFields no context

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,22 @@ import * as graphqlHTTP from 'express-graphql';
 
 import db from './models';
 import schema from './graphql/schema';
-import { extractJwtMiddleware } from './middlewares/extract-jwt.middleware';
+
+import { RequestedFields } from './graphql/ast/RequestedFields';
 import { DataLoaderFactory } from './graphql/dataloaders/DataLoaderFactory';
+import { extractJwtMiddleware } from './middlewares/extract-jwt.middleware';
 
 class App {
   public express: express.Application;
   private dataLoaderFactory: DataLoaderFactory;
+  private requestedFields: RequestedFields;
   constructor() {
     this.express = express();
     this.init();
   }
   private init() {
     this.dataLoaderFactory = new DataLoaderFactory(db);
+    this.requestedFields = new RequestedFields();
     this.middleware();
   }
   private middleware() {
@@ -23,6 +27,7 @@ class App {
       (req: express.Request, res: express.Response, next: express.NextFunction) => {
         req['context']['db'] = db;
         req['context']['dataloaders'] = this.dataLoaderFactory.getLoaders();
+        req['context']['requestedFields'] = this.requestedFields;
         next();
       },
       graphqlHTTP((req) => ({
--- src/interfaces/ResolverContextInterface.ts
+++ src/interfaces/ResolverContextInterface.ts
@@ -1,10 +1,12 @@
-import { DbConnetion } from "./DbConnectionInterface";
 import { AuthUser } from "./AuthUserInterface";
 import { DataLoaders } from "./DataLoadersInterface";
+import { DbConnetion } from "./DbConnectionInterface";
+import { RequestedFields } from "../graphql/ast/RequestedFields";
 
 export interface ResolverContext {
   db?: DbConnetion;
   authorization?: string;
   authUser?: AuthUser;
   dataloaders?: DataLoaders;
+  requestedFields?: RequestedFields;
 }
