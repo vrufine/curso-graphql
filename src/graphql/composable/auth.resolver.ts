ad1c85c6c07a075b9508d99bbceee263bf80fad6
Vinicius Rufine <vsrufine@gmail.com>
2018-02-12 23:12:59 -0200

    76. Composable: authResolver

---
+++ src/graphql/composable/auth.resolver.ts
@@ -0,0 +1,15 @@
+import { GraphQLFieldResolver, GraphQLResolveInfo } from "graphql";
+
+import { ComposableResolver } from "./composable.resolver";
+import { AuthUser } from "../../interfaces/AuthUserInterface";
+import { ResolverContext } from "../../interfaces/ResolverContextInterface";
+
+export const authResolver: ComposableResolver<any, ResolverContext> =
+  (resolver: GraphQLFieldResolver<any, ResolverContext>): GraphQLFieldResolver<any, ResolverContext> => {
+    return (parent, args, context: ResolverContext, info: GraphQLResolveInfo) => {
+      if (context.user || context.authorization) {
+        return resolver(parent, args, context, info);
+      }
+      throw new Error(`Unauthorized! Token not provided!`);
+    }
+  }
