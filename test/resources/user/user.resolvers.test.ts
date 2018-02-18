1592ac42976195342ae79e1305f3cc22e706974e
Vinicius Rufine <vsrufine@gmail.com>
2018-02-18 01:06:42 -0300

    114. User schema: testando as Mutations (parte 1)

--- test/resources/user/user.resolvers.test.ts
+++ test/resources/user/user.resolvers.test.ts
@@ -1,8 +1,12 @@
+import * as jwt from 'jsonwebtoken';
 import { expect, db, app, handleError, chai } from "../../test-utils";
 import { UserInstance } from "../../../src/models/UserModel";
+import { JWT_SECRET } from '../../../src/utils/utils';
 
 describe('User', () => {
   let userId: number;
+  let token: string;
+
   beforeEach(() => {
     return db.Comment.destroy({ where: {} })
       .then(rows => db.Post.destroy({ where: {} }))
@@ -26,6 +30,8 @@ describe('User', () => {
       ]))
       .then((users: UserInstance[]) => {
         userId = users[0].get('id');
+        const payload = { sub: userId };
+        token = jwt.sign(payload, JWT_SECRET);
       })
   })
 
@@ -81,7 +87,7 @@ describe('User', () => {
               expect(paginatedList).to.be.an('array').of.length(2);
               expect(paginatedList[0]).to.have.keys('name', 'email', 'createdAt');
               expect(paginatedList[0]).to.not.have.keys('id', 'updatedAt', 'posts', 'photo');
-            })
+            }).catch(handleError);
         });
       })
       describe('user', () => {
@@ -112,7 +118,7 @@ describe('User', () => {
               expect(user).to.not.have.keys('photo', 'createdAt', 'updatedAt', 'posts');
               expect(user.name).to.eq('Vinícius Rufine');
               expect(user.email).to.eq('vinicius.rufine@atualcs.com.br');
-            })
+            }).catch(handleError);
         })
         it('should return only the "name" attribute', () => {
           const body = {
@@ -139,7 +145,7 @@ describe('User', () => {
               expect(user).to.not.have.keys('id', 'photo', 'createdAt', 'updatedAt', 'posts', 'email');
               expect(user.name).to.eq('Vinícius Rufine');
               expect(user.email).to.be.undefined;
-            })
+            }).catch(handleError);
         })
         it('should return an error if User dont\'t exist', () => {
           const body = {
@@ -163,6 +169,83 @@ describe('User', () => {
               expect(res.body.errors).to.be.an('array');
               expect(res.body).to.have.keys('data', 'errors');
               expect(res.body.errors[0].message).to.eq('Error: User with id -1 not found!');
+            }).catch(handleError);
+        })
+      })
+    })
+  })
+
+  describe('Mutations', () => {
+    describe('application/json', () => {
+      describe('createUser', () => {
+        it('should create a new User', () => {
+          const body = {
+            query: `
+              mutation createNewUser($input: UserCreateInput!) {
+                createUser(input: $input) {
+                  id
+                  name
+                  email
+                }
+              }
+            `,
+            variables: {
+              input: {
+                name: 'Heitor Dobeis',
+                email: 'heitor@atualcs.com.br',
+                password: '123456'
+              }
+            }
+          };
+          return chai.request(app)
+            .post('/graphql')
+            .set('content-type', 'application/json')
+            .send(JSON.stringify(body))
+            .then(res => {
+              let newUser = res.body.data.createUser;
+              expect(res.body.data).to.be.an('object');
+              expect(newUser).to.be.an('object');
+              expect(newUser).to.have.keys('id', 'name', 'email');
+              expect(newUser).to.not.have.keys('photo', 'createdAt', 'updatedAt', 'posts');
+              expect(parseInt(newUser.id)).to.be.a('number');
+              expect(newUser.name).to.eq('Heitor Dobeis');
+              expect(newUser.email).to.eq('heitor@atualcs.com.br');
+            }).catch(handleError)
+        })
+      })
+      describe('updateUser', () => {
+        it('should update an existing User', () => {
+          const body = {
+            query: `
+              mutation updateExistingUser($input: UserUpdateInput!) {
+                updateUser(input: $input) {
+                  name
+                  email
+                  photo
+                }
+              }
+            `,
+            variables: {
+              input: {
+                name: 'Vinícius S. Rufine',
+                email: 'vsrufine@gmail.com',
+                photo: 'photo_base_64'
+              }
+            }
+          };
+          return chai.request(app)
+            .post('/graphql')
+            .set('content-type', 'application/json')
+            .set('authorization', `Bearer ${token}`)
+            .send(JSON.stringify(body))
+            .then(res => {
+              const updatedUser = res.body.data.updateUser;
+              expect(updatedUser).to.be.an('object');
+              expect(updatedUser).to.have.keys('name', 'email', 'photo');
+              expect(updatedUser.name).to.eq('Vinícius S. Rufine');
+              expect(updatedUser.email).to.eq('vsrufine@gmail.com');
+              expect(updatedUser.photo).to.not.be.null;
+              expect(updatedUser.id).to.be.undefined;
             })
         })
       })
