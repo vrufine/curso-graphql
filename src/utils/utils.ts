c048e7a1c27f4964be1ba7c26028be9d633bcdee
Vinicius Rufine <vsrufine@gmail.com>
2018-02-18 18:27:18 -0300

    Correção para JWT_SECRET

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -47,4 +47,4 @@ export const throwError = (condition: boolean, message: string): void => {
   if (condition) throw new Error(message);
 }
 
-export const JWT_SECRET: string = process.env.JWT_SECRET;
+export const JWT_SECRET: string = process.env.JWT_SECRET || '3ss4p4$$w0rdEHz1k4';
